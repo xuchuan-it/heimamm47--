fb4ce817bf1bd85eb092362ee3156ae1afe1e722
xuhaoran <1728291767@qq.com>
2020-03-05 16:40:03 +0800

    Revert "完成权限分支"
    This reverts commit a28526927230c4e07a0e993eff21f6e6f75f4774.

--- src/router/childrenRoutes.js
+++ src/router/childrenRoutes.js
@@ -9,53 +9,44 @@ import subject from '../views/index/subject/subject.vue'
 export default [
     // 子路由一般不加/
     {
-        path: "chart",
-        component: chart,
-        meta: {
-            title: '数据概览',
-            roles: ['超级管理员', '管理员'],
-            icon: "el-icon-pie-chart"
-        }
+      path: "user",
+      component: user,
+      meta: {
+        title: '用户列表',
+        roles: ['超级管理员', '管理员']
+      },
+     
     },
-
     {
-        path: "user",
-        component: user,
-        meta: {
-            title: '用户列表',
-            roles: ['超级管理员', '管理员'],
-            icon: "el-icon-user"
-        },
-
+      path: "chart",
+      component: chart,
+      meta: {
+        title: '数据概览',
+        roles: ['超级管理员', '管理员']
+      }
     },
-
     {
-        path: "question",
-        component: question,
-        meta: {
-            title: '题库列表',
-            roles: ['超级管理员', '管理员', '老师', '学生'],
-            icon: "el-icon-edit-outline"
-        }
+      path: "business",
+      component: business,
+      meta: {
+        title: '企业列表',
+        roles: ['超级管理员', '管理员', '老师']
+      }
     },
-
     {
-        path: "business",
-        component: business,
-        meta: {
-            title: '企业列表',
-            roles: ['超级管理员', '管理员', '老师'],
-            icon: "el-icon-office-building"
-        }
+      path: "question",
+      component: question,
+      meta: {
+        title: '题库列表',
+        roles: ['超级管理员', '管理员', '老师', '学生']
+      }
     },
-
     {
-        path: "subject",
-        component: subject,
-        meta: {
-            title: '学科列表',
-            roles: ['超级管理员', '管理员', '老师', '学生'],
-            icon: "el-icon-notebook-2"
-        }
+      path: "subject",
+      component: subject,
+      meta: {
+        title: '学科列表',
+        roles: ['超级管理员', '管理员', '老师', '学生']
+      }
     },
-]
+  ]
